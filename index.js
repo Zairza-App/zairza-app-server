36818352c7b957769bfc01e53b5ca641f9bc7e74
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2023-03-11 13:32:30 +0530

    add : cors and exported express app

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const auth = require('./middlewares/auth');
+const cors = require("cors")
 
 
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 mongoose.set("strictQuery", false);
@@ -30,3 +32,5 @@ app.use('/', routes);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
