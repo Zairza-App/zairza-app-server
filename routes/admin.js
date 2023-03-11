9dbbbbfeba8a6aa362cd419e3b8392b5b557198f
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2023-03-12 01:04:03 +0530

    update: fix session

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Session = require('../models/Session');
+const Session = require('../models/session');
 
 router.post('/sessions', async (req, res) => {
     try {
